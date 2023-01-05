5d0fed66879437ec0e63f0533c19485f2c7f042a
Kevin Yu <kevinwcyu@users.noreply.github.com>
2023-01-05 13:09:24 -0800

    use .config dir for base configuration

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,4 @@
 module.exports = {
-  endOfLine: 'auto',
-  printWidth: 120,
-  trailingComma: 'es5',
-  semi: true,
-  jsxSingleQuote: false,
-  singleQuote: true,
-  useTabs: false,
-  tabWidth: 2,
+  // Prettier configuration provided by Grafana scaffolding
+  ...require('./.config/.prettierrc.js'),
 };
