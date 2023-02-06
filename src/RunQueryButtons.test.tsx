5731f25164079b191bfd3e4e2880f088e9712864
Ida Štambuk <ida.stambuk@grafana.com>
2023-02-06 10:32:23 +0100

    Make query buttons into header buttons

--- src/RunQueryButtons.test.tsx
+++ src/RunQueryButtons.test.tsx
@@ -8,7 +8,7 @@ const getDefaultProps = (overrides?: Partial<RunQueryButtonsProps<DataQuery>>) =
   return {
     onRunQuery: jest.fn(),
     onCancelQuery: jest.fn(),
-    isQueryValid: jest.fn(),
+    enableRun: true,
     query: { refId: 'refId' },
     ...overrides,
   };
@@ -16,34 +16,34 @@ const getDefaultProps = (overrides?: Partial<RunQueryButtonsProps<DataQuery>>) =
 
 describe('RunQueryButtons', () => {
   it('disable the run button if the query is invalid', () => {
-    const props = getDefaultProps({ isQueryValid: jest.fn().mockReturnValue(false) });
+    const props = getDefaultProps({ enableRun: false});
     render(<RunQueryButtons {...props} />);
-    const runButton = screen.getByRole('button', { name: 'Run' });
+    const runButton = screen.getByRole('button', { name: 'Run query' });
     expect(runButton).toBeDisabled();
   });
 
   it('run button should be enabled if the query is valid', () => {
-    const props = getDefaultProps({ isQueryValid: jest.fn().mockReturnValue(true) });
+    const props = getDefaultProps({ enableRun: true});
     render(<RunQueryButtons {...props} />);
-    const runButton = screen.getByRole('button', { name: 'Run' });
+    const runButton = screen.getByRole('button', { name: 'Run query' });
     expect(runButton).not.toBeDisabled();
   });
 
   it('only renders the `Run` button if onCancelQuery is undefined', () => {
     const props = getDefaultProps({ onCancelQuery: undefined });
     render(<RunQueryButtons {...props} />);
-    const runButton = screen.getByRole('button', { name: 'Run' });
+    const runButton = screen.getByRole('button', { name: 'Run query' });
     expect(runButton).toBeInTheDocument();
-    const stopButton = screen.queryByRole('button', { name: 'Stop' });
+    const stopButton = screen.queryByRole('button', { name: 'Stop query' });
     expect(stopButton).not.toBeInTheDocument();
   });
 
   it('renders the `Run` and `Stop` buttons if onCancelQuery defined', () => {
     const props = getDefaultProps();
     render(<RunQueryButtons {...props} />);
-    const runButton = screen.getByRole('button', { name: 'Run' });
+    const runButton = screen.getByRole('button', { name: 'Run query' });
     expect(runButton).toBeInTheDocument();
-    const stopButton = screen.queryByRole('button', { name: 'Stop' });
+    const stopButton = screen.queryByRole('button', { name: 'Stop query' });
     expect(stopButton).toBeInTheDocument();
   });
 });
